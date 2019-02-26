6afd7d1b7ba237de33f07b859a2f7d767cf7f883
SaroshKhawar496 <sarosh.496@gmail.com>
2019-02-26 15:59:46 -0500

    Created recipe component to show recipes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Form from './components/Form'
+import Form from './components/Form';
+import Recipes from './components/Recipes';
 
 const RECIPE_API_KEY = '69636edda28df589735b58857ee097cc';
 
 
 class App extends Component {
+  state = {
+    recipes: []
+  }
 
   //making api call using async await
   getRecipe = async (e) => {
     // reading the recipe name from the search text box
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault(); //prevents the full page refresh!
-    const api_call = await fetch(`https://www.food2fork.com/api/search?key=${RECIPE_API_KEY}&q=chicken&count=5`);
+    const api_call = await fetch(`https://www.food2fork.com/api/search?key=${RECIPE_API_KEY}&q=${recipeName}&count=5`);
 
     const data = await api_call.json();
 
-    console.log(data);
+    this.setState({recipes: data.recipes});
+
+    console.log(this.state);
 
   }
 
@@ -29,6 +35,8 @@ class App extends Component {
         </header>
 
         <Form getRecipe={this.getRecipe}/>
+        <Recipes recipes={this.state.recipes} />
+
 
       </div>
     );
